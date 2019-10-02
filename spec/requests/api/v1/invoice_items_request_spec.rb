c6f89778710e7a2fe188892cb883038b3c5447ac
Corina Allen <StarPerfect@gmail.com>
2019-10-01 21:13:13 -0600

    Update Request Specs for Invoice and Customers

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe "Invoice Items API" do
   it "sends a list of invoice items" do
-    create_list(:invoice_item, 3)
+    item = create(:item)
+    invoice = create(:invoice)
+    create_list(:invoice_item, 3, invoice: invoice, item: item)
 
     get '/api/v1/invoice_items'
 
