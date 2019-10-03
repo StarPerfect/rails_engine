63dc5b1077e0fb18d2ade5f95354cca8437e4ee0
Corina Allen <StarPerfect@gmail.com>
2019-10-03 13:09:03 -0600

    Add Merchants route to /find functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#find'
+      end
+    end
+  end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :customers, only: [:index, :show]
+
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], to: 'merchants/items#index'
+      end
+
+      resources :invoices, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+    end
+  end
 end
