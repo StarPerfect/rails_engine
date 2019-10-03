97292db4262cc19fb0d2a0a1cb3de2552f826997
Corina Allen <StarPerfect@gmail.com>
2019-10-03 12:41:41 -0600

    Add tests with SearchController to searh for a single merchant

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::Merchants::SearchController < ApplicationController
+
+  def find
+    if params[:id]
+      # binding.pry
+      merchant = Merchant.find_by(find_params)
+    elsif params[:name]
+      merchant = Merchant.find_by( 'lower(name) = ?' , params[:name].downcase )
+    elsif params[:created_at]
+      merchant = Merchant.find_by(find_params)
+    elsif params[:updated_at]
+      merchant = Merchant.find_by(find_params)
+    end
+    render json: MerchantSerializer.new(merchant)
+    # DRY up with: render json: MerchantSerializer.new(Merchant.find_by(find_params))
+  end
+
+private
+  def find_params
+    params.permit(:name, :id, :created_at, :updated_at)
+  end
+end
---
+++ spec/requests/api/v1/merchants/find_merchant_spec.rb
@@ -0,0 +1,98 @@
+require 'rails_helper'
+
+describe 'Merchants Search API' do
+  it 'find a single merchants based on id' do
+    merchant = create(:merchant, name: 'FirstTest')
+
+    get "/api/v1/merchants/find?id=#{merchant.id}"
+
+    expected = {
+                "data": {
+                  "id": "#{merchant.id}",
+                  "type": "merchant",
+                  "attributes": {
+                    "name": "FirstTest"
+                  }
+                }
+              }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single merchants based on name' do
+    merchant = create(:merchant, name: 'SecondTest')
+
+    get "/api/v1/merchants/find?name=#{merchant.name}"
+
+    expected = {
+                "data": {
+                  "id": "#{merchant.id}",
+                  "type": "merchant",
+                  "attributes": {
+                    "name": "SecondTest"
+                  }
+                }
+              }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single merchants based on name case independent' do
+    merchant = create(:merchant, name: 'Queen Bee')
+
+    get "/api/v1/merchants/find?name=#{merchant.name.downcase}"
+
+    expected = {
+                "data": {
+                  "id": "#{merchant.id}",
+                  "type": "merchant",
+                  "attributes": {
+                    "name": "Queen Bee"
+                  }
+                }
+              }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single merchants based on created timestamp' do
+    merchant = create(:merchant, name: 'FourthTest')
+
+    get "/api/v1/merchants/find?created_at=#{merchant.created_at}"
+
+    expected = {
+                "data": {
+                  "id": "#{merchant.id}",
+                  "type": "merchant",
+                  "attributes": {
+                    "name": "FourthTest"
+                  }
+                }
+              }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single merchants based on updated timestamp' do
+    merchant = create(:merchant, name: 'FifthTest')
+
+    get "/api/v1/merchants/find?updated_at=#{merchant.updated_at}"
+
+    expected = {
+                "data": {
+                  "id": "#{merchant.id}",
+                  "type": "merchant",
+                  "attributes": {
+                    "name": "FifthTest"
+                  }
+                }
+              }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+end
