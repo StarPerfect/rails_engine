6f68e5daa06dc6e959e323044e236b85a8da2545
Corina Allen <StarPerfect@gmail.com>
2019-10-03 19:22:59 -0600

    Change order of find params

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -1,18 +1,6 @@
 class Api::V1::InvoiceItems::SearchController < ApplicationController
 
   def find
-    # if params[:id]
-    #   customer = Customer.find_by(find_params)
-    # elsif params[:quantity]
-    #   customer = Customer.find_by( 'lower(first_name) = ?' , params[:first_name].downcase )
-    # elsif params[:last_name]
-    #   customer = Customer.find_by( 'lower(last_name) = ?' , params[:last_name].downcase )
-    # elsif params[:created_at]
-    #   customer = Customer.find_by(find_params)
-    # elsif params[:updated_at]
-    #   customer = Customer.find_by(find_params)
-    # end
-    # render json: CustomerSerializer.new(customer)
     render json: InvoiceItemSerializer.new(InvoiceItem.find_by(find_params))
   end
 
