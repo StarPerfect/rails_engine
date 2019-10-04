3d407642e53a95a31f413555be005ea465561e9f
Corina Allen <StarPerfect@gmail.com>
2019-10-03 19:21:24 -0600

    Complete single invoice search functionality

---
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::Invoices::SearchController < ApplicationController
+  def find
+    render json: InvoiceSerializer.new(Invoice.find_by(find_params))
+  end
+
+private
+  def find_params
+    params.permit(:id, :customer_id, :merchant_id, :status, :created_at, :updated_at)
+  end
+end
---
+++ spec/requests/api/v1/invoices/find_invoice_spec.rb
@@ -0,0 +1,147 @@
+require 'rails_helper'
+
+describe 'Invoice Search API' do
+  it 'find a single invoice based on id' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+
+    get "/api/v1/invoices/find?id=#{invoice.id}"
+
+    expected = {
+                data: {
+                  id: "#{invoice.id}",
+                  type: "invoice",
+                  attributes: {
+                    id: invoice.id,
+                    customer_id: customer.id,
+                    merchant_id: merchant.id,
+                    status: "MyString",
+                    },
+                  }
+                }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single item_items based on customer id' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+
+    get "/api/v1/invoices/find?customer_id=#{invoice.customer_id}"
+
+    expected = {
+                data: {
+                  id: "#{invoice.id}",
+                  type: "invoice",
+                  attributes: {
+                    id: invoice.id,
+                    customer_id: customer.id,
+                    merchant_id: merchant.id,
+                    status: "MyString",
+                    },
+                  }
+                }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single item_items based on merchant id' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+
+    get "/api/v1/invoices/find?merchant_id=#{invoice.merchant_id}"
+
+    expected = {
+                data: {
+                  id: "#{invoice.id}",
+                  type: "invoice",
+                  attributes: {
+                    id: invoice.id,
+                    customer_id: customer.id,
+                    merchant_id: merchant.id,
+                    status: "MyString",
+                    },
+                  }
+                }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'can find a single item item based on status' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+
+    get "/api/v1/invoices/find?status=#{invoice.status}"
+
+    expected = {
+                data: {
+                  id: "#{invoice.id}",
+                  type: "invoice",
+                  attributes: {
+                    id: invoice.id,
+                    customer_id: customer.id,
+                    merchant_id: merchant.id,
+                    status: "MyString",
+                    },
+                  }
+                }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single item_items based on created timestamp' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+
+    get "/api/v1/invoices/find?created_at=#{invoice.created_at}"
+
+    expected = {
+                data: {
+                  id: "#{invoice.id}",
+                  type: "invoice",
+                  attributes: {
+                    id: invoice.id,
+                    customer_id: customer.id,
+                    merchant_id: merchant.id,
+                    status: "MyString",
+                    },
+                  }
+                }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it 'find a single item_items based on updated timestamp' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, customer: customer, merchant: merchant)
+
+    get "/api/v1/invoices/find?updated_at=#{invoice.updated_at}"
+
+    expected = {
+                data: {
+                  id: "#{invoice.id}",
+                  type: "invoice",
+                  attributes: {
+                    id: invoice.id,
+                    customer_id: customer.id,
+                    merchant_id: merchant.id,
+                    status: "MyString",
+                    },
+                  }
+                }
+
+    expect(response).to be_successful
+    expect(response.body).to eq(expected.to_json)
+  end
+end
