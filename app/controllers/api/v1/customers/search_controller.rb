e8bbcc0b0a69ed338567e061c73ac91c2965deef
Corina Allen <StarPerfect@gmail.com>
2019-10-05 10:53:12 -0600

    Refactor Search#Find for Customers

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,19 +1,26 @@
 class Api::V1::Customers::SearchController < ApplicationController
 
   def find
-    if params[:id]
-      customer = Customer.find_by(find_params)
-    elsif params[:first_name]
+    if params[:first_name]
       customer = Customer.find_by( 'lower(first_name) = ?' , params[:first_name].downcase )
     elsif params[:last_name]
       customer = Customer.find_by( 'lower(last_name) = ?' , params[:last_name].downcase )
-    elsif params[:created_at]
-      customer = Customer.find_by(find_params)
-    elsif params[:updated_at]
+    else
       customer = Customer.find_by(find_params)
     end
     render json: CustomerSerializer.new(customer)
     # DRY up with: render json: CustomerSerializer.new(Customer.find_by(find_params))
+  end
+
+  def find_all
+    if params[:first_name]
+      customer = Customer.where( 'lower(first_name) = ?' , params[:first_name].downcase )
+    elsif params[:last_name]
+      customer = Customer.where( 'lower(last_name) = ?' , params[:last_name].downcase )
+    else
+      customer = Customer.where(find_params)
+    end
+    render json: CustomerSerializer.new(customer)
   end
 
 private
