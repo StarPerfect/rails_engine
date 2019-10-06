04fbecfa459182fb1ee3b95afa9f97f5c517f708
Corina Allen <StarPerfect@gmail.com>
2019-10-06 13:52:34 -0600

    Complete Merchants/:id/Invoices nested endpoint with Merchants::InvoicesController and InvoiceSerializer

--- spec/requests/api/v1/merchants/invoices_endpoint_spec.rb
+++ spec/requests/api/v1/merchants/invoices_endpoint_spec.rb
@@ -4,11 +4,13 @@ describe 'Merchants Invoices API' do
   it 'displays all invoices for a particular merchant' do
     customer_1 = create(:customer)
     customer_2 = create(:customer)
-    merchant = create(:merchant)
-    invoice_1 = create(:invoice, customer: customer_1, merchant: merchant)
-    invoice_2 = create(:invoice, customer: customer_2, merchant: merchant)
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    invoice_1 = create(:invoice, customer: customer_1, merchant: merchant_1)
+    invoice_2 = create(:invoice, customer: customer_2, merchant: merchant_1)
+    invoice_3 = create(:invoice, customer: customer_2, merchant: merchant_2)
 
-    get "/api/v1/merchants/#{merchant.id}/invoices"
+    get "/api/v1/merchants/#{merchant_1.id}/invoices"
 
     expected = {
                 data: [{
@@ -17,7 +19,7 @@ describe 'Merchants Invoices API' do
                   attributes: {
                     id: invoice_1.id,
                     customer_id: customer_1.id,
-                    merchant_id: merchant.id,
+                    merchant_id: merchant_1.id,
                     status: "MyString",
                     },
                   }, {
@@ -26,13 +28,20 @@ describe 'Merchants Invoices API' do
                     attributes: {
                       id: invoice_2.id,
                       customer_id: customer_2.id,
-                      merchant_id: merchant.id,
+                      merchant_id: merchant_1.id,
                       status: "MyString",
                       },
                   }]
                 }
 
+    invoices = JSON.parse(response.body)
+
     expect(response).to be_successful
     expect(response.body).to eq(expected.to_json)
+    expect(invoices["data"].count).to eq(2)
+    expect(invoices["data"][0]["id"]).to eq("#{invoice_1.id}")
+    expect(invoices["data"][1]["id"]).to eq("#{invoice_2.id}")
+    expect(invoices["data"][0]["attributes"]["merchant_id"]).to eq(merchant_1.id)
+    expect(invoices["data"][1]["attributes"]["merchant_id"]).to eq(merchant_1.id)
   end
 end
